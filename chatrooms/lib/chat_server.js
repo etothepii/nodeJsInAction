a1ef06c5396a13c3b5eeb42d4626bbe5fff4559c
James Robinson <c9@epii.co.uk>
2014-01-19 12:28:56 -0500

    Transcription Error

--- chatrooms/lib/chat_server.js
+++ chatrooms/lib/chat_server.js
@@ -9,7 +9,7 @@ exports.listen = function(server) {
     io = socketio.listen(server);
     io.set('log level', 1);
     io.sockets.on('connection', function(socket) {
-        guestNumber = assignGuestName(socket, guestName, nickNames, namesUsed);
+        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
         joinRoom(socket, 'Lobby');
         handleMessageBroadcasting(socket, nickNames);
         handleNameChangeAttempts(socket, nickNames, namesUsed);
